a06065fd088d7964a3923a58c42cfb357e9e13b5
Uttam Kumar <utt.1234@gmail.com>
2026-05-10 20:13:15 +0530

    modified testing

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -4,18 +4,24 @@ import java.util.stream.IntStream;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String str = "racecar madam";
-        str.equals(new StringBuilder(str).reverse().toString());
+        //rev of word is a palindrome
+        String str = "racecar";
+        //tostring can be replace by contentEquals
+        System.out.println(str.contentEquals(new StringBuilder(str).reverse()) ?
+               "palindrome" : "not palindrome");
+
+        //reversing whole word and checking palindrome or not
         String rev = IntStream.range(0, str.length())
                 .mapToObj(i -> String.valueOf(str.charAt(str.length()-1-i)))
                 .collect(Collectors.joining());
         System.out.println(rev.equals(str) ? "palindrome "+ rev : "not palindrome " + rev);
 
+        //every word is a palindrome or not
         String words = Arrays.stream(str.split("\\s+"))
                 .map(w -> IntStream.range(0, w.length())
                         .mapToObj(j -> String.valueOf(w.charAt(w.length()-1-j)))
                         .collect(Collectors.joining())).collect(Collectors.joining(" "));
-        System.out.println(words.equals(str) ? "palindrome "+ words : "not palindrome " + words);
+        System.out.println(words.equals(str) ? "palindrome "+ words +"hello" : "not palindrome " + words);
 
     }
 }
