96823d659c4a72fbb3d5ef4f6208901b64a3d00d
Uttam Kumar <utt.1234@gmail.com>
2026-05-10 20:47:42 +0530

    modified testing

---
+++ src/main/java/PalindromeVariant.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class PalindromeVariant {
+    public static void main(String[] args) {
+        String word = "racecar";
+        //every word is a palindrome just check previous variant has reverse logic
+        boolean b = IntStream.range(0, word.length()/2)
+                .allMatch(i -> word.charAt(i) == word.charAt(word.length()-1-i));
+        System.out.println(b ? "all words palindrome" : "words not palindrome");
+
+        boolean rev = Arrays.stream(word.split("//s+"))
+                .allMatch(w -> IntStream.range(0, w.length()/2)
+                        .allMatch(i -> w.charAt(i) == w.charAt(w.length()-1-i)));
+        System.out.println(rev ? "all words palindrome" : "words not palindrome");
+
+    }
+}
